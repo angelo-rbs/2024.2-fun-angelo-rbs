695b626ef87f90daebe942faf5bc3f362564d272
angelo-rbs <angeloraphael1@hotmail.com>
2024-10-13 17:35:14 -0300

    read: hutton, chapter 3

--- haskell/hws/q1.hs
+++ haskell/hws/q1.hs
@@ -1,7 +1,7 @@
 module Nat where
 
 import Distribution.Compat.CharParsing (Parsing (unexpected))
-import Prelude hiding (Num (..), max, min, pred)
+import Prelude hiding (Num (..), gcd, max, min, mod, pred, (<), (>))
 
 data Nat = O | S Nat
   deriving (Eq, Show)
@@ -35,14 +35,16 @@ pred :: Nat -> Nat
 pred O = O
 pred (S n) = n
 
--- questão 8
+-- item 8
 
 fact :: Nat -> Nat
 fact O = S O
 fact (S n) = S n * fact n
 
 fib :: Nat -> Nat
-fib = undefined
+fib O = S O
+fib (S O) = S O
+fib (S (S n)) = fib (S n) + fib n
 
 min :: (Nat, Nat) -> Nat
 min (n, O) = O
@@ -54,8 +56,35 @@ max (n, O) = n
 max (O, n) = n
 max (S n, S m) = S (max (n, m))
 
+-- auxiliares pras funçõe seguintes
+
+-- natMinus :: Nat -> Nat -> Nat
+
+(>) :: Nat -> Nat -> Bool
+(>) (S _) O = True
+(>) O (S _) = False
+(>) (S m) (S n) =
+  if m == n
+    then False
+    else (>) m n
+
+(<) :: Nat -> Nat -> Bool
+(<) (S _) O = False
+(<) O (S _) = True
+(<) (S m) (S n) =
+  if m == n
+    then False
+    else (<) m n
+
+-- fim funções auxiliares
+
 div :: Nat -> Nat -> (Nat, Nat)
-div = undefined
+div _ O = error "division by zero"
+div O n = (O, n)
+div (S m) (S n)
+  | m == n = (S O, O)
+  | m > n =
+  | n > m = (O, m)
 
 quot :: (Nat, Nat) -> Nat
 quot = undefined
@@ -64,7 +93,10 @@ rem :: (Nat, Nat) -> Nat
 rem = undefined
 
 gcd :: (Nat, Nat) -> Nat
-gcd = undefined
+gcd (m, O) = m
+gcd (m, n) = undefined -- gcd (n, mod m n)
+
+--
 
 gcm :: (Nat, Nat) -> Nat
 gcm = undefined
